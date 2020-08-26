8c2c601ccfdd4f3755643df1aed59205479f8059
Przemysław Juśkiewicz <nyweron@gmail.com>
2020-08-26 20:32:20 +0200

    Added backend urls

--- src/shared/apiUrl.js
+++ src/shared/apiUrl.js
@@ -1,3 +1,6 @@
 export const expenseUrl = "http://localhost:8081/expense"
 export const revenueUrl = "http://localhost:8081/revenue"
 export const savingUrl = "http://localhost:8081/saving"
+
+export const mockBackendUrl = "http://localhost:8081/"
+export const backendUrl = "http://localhost:54101/api/"
