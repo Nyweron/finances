ab01dfeb6386aa075f3ce1609bd82ed5c6054f52
Przemysław Juśkiewicz <nyweron@gmail.com>
2022-02-08 20:46:47 +0100

    Create saving

---
+++ src/component/saving/index.js
@@ -0,0 +1,9 @@
+import SavingAdd from './savingAdd';
+import SavingEdit from './savingEdit';
+import SavingRemove from './savingRemove';
+
+export {
+  SavingAdd,
+  SavingEdit,
+  SavingRemove,
+}
--- src/pages/saving.js
+++ src/pages/saving.js
@@ -1,139 +1,224 @@
 import React, { Component } from "react";
 
-import SavingFilter from "../component/saving/savingFilter";
-import SavingEdit from "../component/saving/savingEdit";
-import SavingAdd from "../component/saving/savingAdd";
-import TableContainer from "../component/myCustomCRUDTable/TableContainer";
+import { Table, Icon, Pagination } from "semantic-ui-react";
 
-import {getAll} from "../lib/savingService";
-import {getKeyFromJson} from "../lib/crudHelper";
+import {
+  getAll,
+  createSaving,
+  editPutSaving,
+  deleteRowSaving,
+} from "../lib/savingService";
 
-import styles from "../App.module.css";
+import {
+  SavingAdd,
+  SavingEdit,
+  SavingRemove,
+} from "../component/saving";
 
 
 class Saving extends Component {
-  state = {
-    isVisibleFilterSettings: false,
-    data: null,
-    columns: null
-  };
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      allData: [],
+      dataEdit: {},
+      dataRemove: {},
+      savingDataOnPage: [],
+      begin: 0,
+      end: 4,
+      showModalAdd: false,
+      showModalEdit: false,
+      showModalRemove: false,
+      isCreated: false,
+      isEdited: false,
+      isRemoved: false,
+    };
+  }
 
-  componentDidMount(){
-    getAll("saving").then(rows => {
-      console.log("🚀 ~ file: saving.js ~ line 23 ~ Saving ~ getAll ~ rows", rows)
-      this.setState({ data: rows });
-      const keys = getKeyFromJson(rows);
-      if (keys !== null) {
-        this.setState({ columns: keys });
-      }
+  componentDidMount() {
+    getAll("saving").then((rows) => {
+      this.setState({
+        allData: rows,
+        savingDataOnPage: rows.slice(this.state.begin, this.state.end),
+      });
     });
   }
 
-  displayFilterSettings = () => {
+  componentDidUpdate() {
+    if (this.state.isCreated || this.state.isEdited || this.state.isRemoved) {
+      getAll("saving")
+        .then((rows) => {
+          this.setState({
+            allData: rows,
+            savingDataOnPage: rows.slice(this.state.begin, this.state.end),
+            isCreated: false,
+            isEdited: false,
+            isRemoved: false,
+          });
+        })
+        .catch((error) => {
+          this.setState({
+            isCreated: false,
+            isEdited: false,
+            isRemoved: false,
+          });
+        });
+    }
+  }
+
+  onChangePage = async (event: React.MouseEvent<HTMLAnchorElement>, data) => {
+    await this.setState({
+      activePage: data.activePage,
+      begin: data.activePage * 4 - 4,
+      end: data.activePage * 4,
+    });
+
     this.setState({
-      isVisibleFilterSettings: !this.state.isVisibleFilterSettings
+      savingDataOnPage: this.state.allData.slice(
+        this.state.begin,
+        this.state.end
+      ),
     });
   };
 
-  addSaving = addObj => {
-    console.log("saving.js addSaving", addObj);
-
-    // if (
-    //   addObj === undefined ||
-    //   addObj === null ||
-    //   addObj.firstName === null ||
-    //   addObj.firstName === undefined ||
-    //   addObj.firstName === ""
-    // ) {
-    //   this.showTempMessage("Firstname is required");
-    //   return;
-    // }
-
-    // const allRows = this.state.rowsFromDbJson;
-    // const sortedIds = sortIds(allRows);
-    // if (sortedIds.length === 0) {
-    //   sortedIds.push("");
-    // }
-    // const newId = generateNewId(sortedIds);
-
-    // const newPerson = {
-    //   id: newId,
-    //   firstName: addObj.firstName,
-    //   lastName: addObj.lastName,
-    //   age: addObj.age,
-    //   isActive: true,
-    //   hobby: addObj.hobby
-    // };
-
-    // createPerson(newPerson).then(
-    //   () => this.showTempMessage("person created"),
-    //   this.setState(
-    //     {
-    //       rowsFromDbJson: [...this.state.rowsFromDbJson, newPerson]
-    //     },
-    //     () => {
-    //       this.invokePaginationOnPageChanged();
-    //     }
-    //   )
-    // );
-
-    // for (var key in addObj) {
-    //   delete addObj[key];
-    // }
-  };
-
-  removeSaving = id => {
-    console.log("saving.js removeSaving", id);
-
-    // let listOfRows = this.state.rowsFromDbJson;
-    // const newListWithoutRemovedItem = removeRowById(listOfRows, id);
-
-    // deleteRow(id).then(
-    //   () => this.showTempMessage("row deleted"),
-    //   this.setState({ rowsFromDbJson: newListWithoutRemovedItem }, () => {
-    //     this.invokePaginationOnPageChanged();
-    //   })
-    // );
-  };
-
-  editSaving = editObj => {
-    console.log("saving.js editSaving", editObj);
-  };
-
   render() {
-    console.log("🚀 ~ file: saving.js ~ line 105 ~ Saving ~ render ~ this.state.data", this.state.data)
-    if(this.state.data === null || this.state.columns === null){
-      return null;
-    }
+
 
     return (
       <>
-        <div className={styles.row}>
-          <div className={styles.card}>
-            <button
-              className="btn btn-primary"
-              onClick={this.displayFilterSettings}
-            >
-              Filter settings savings
-            </button>
-            {this.state.isVisibleFilterSettings && <SavingFilter />}
-          </div>
-          <div className={styles.centerColumn}>
-            <div className={styles.card}>
-              <TableContainer
-                columns={this.state.columns}
-                data={this.state.data}
-                addRow={this.addSaving}
-                removeRow={this.removeSaving}
-                editRow={this.editSaving}
-                EditComponent={SavingEdit}
-                AddComponent={SavingAdd}
-              />
-            </div>
-            <div className={styles.card}>
-              <span>PAGINATION</span>
+        <div className="ui centered grid">
+          <div className="row"></div>
+          <div className="row">
+            <div className="fourteen wide column">
+              <button
+                className="ui blue button"
+                onClick={() => this.handleOpenModalAddExpense()}
+              >
+                Dodaj oszczędności
+              </button>
             </div>
           </div>
+          <div className="row">
+            <div className="fourteen wide column">
+              <Table celled selectable>
+                <Table.Header>
+                  <Table.Row>
+                    <Table.HeaderCell>Id</Table.HeaderCell>
+                    <Table.HeaderCell>Rodzaj</Table.HeaderCell>
+                    <Table.HeaderCell>Kwota</Table.HeaderCell>
+                    <Table.HeaderCell>Stan na dzień</Table.HeaderCell>
+                    <Table.HeaderCell>Komentarz</Table.HeaderCell>
+                    <Table.HeaderCell>Usuń</Table.HeaderCell>
+                    <Table.HeaderCell>Edytuj</Table.HeaderCell>
+                  </Table.Row>
+                </Table.Header>
+
+                <Table.Body>
+                  {this.state.savingDataOnPage.map((item, i) => {
+                    return (
+                      <Table.Row key={`savingRow_${i}`}>
+                        <Table.Cell key={`id${i}`}>{item.id}</Table.Cell>
+                        <Table.Cell key={`categorySavingDescription_${i}`}>
+                          {item.categorySavingDescription}
+                        </Table.Cell>
+                        <Table.Cell key={`howMuch${i}`}>
+                          {item.howMuch}
+                        </Table.Cell>
+                        <Table.Cell key={`date_${i}`}>
+                          {item.date}
+                        </Table.Cell>
+                        <Table.Cell key={`comment_${i}`}>
+                          {item.comment}
+                        </Table.Cell>
+                        <Table.Cell
+                          key={`remove_${i}`}
+                          className="center aligned"
+                        >
+                          <button
+                            className="ui red button"
+                            onClick={() =>
+                              this.handleOpenModalRemoveExpense(item)
+                            }
+                          >
+                            Usuń
+                          </button>
+                        </Table.Cell>
+                        <Table.Cell
+                          key={`Edit_${i}`}
+                          className="center aligned"
+                        >
+                          <button
+                            className="ui green button "
+                            onClick={() =>
+                              this.handleOpenModalEditExpense(item)
+                            }
+                          >
+                            Edytuj
+                          </button>
+                        </Table.Cell>
+                      </Table.Row>
+                    );
+                  })}
+                </Table.Body>
+
+                <Table.Footer>
+                  <Table.Row>
+                    <Table.HeaderCell colSpan={10}>
+                      <Pagination
+                        ellipsisItem={{
+                          content: <Icon name="ellipsis horizontal" />,
+                          icon: true,
+                        }}
+                        firstItem={{
+                          content: <Icon name="angle double left" />,
+                          icon: true,
+                        }}
+                        lastItem={{
+                          content: <Icon name="angle double right" />,
+                          icon: true,
+                        }}
+                        prevItem={{
+                          content: <Icon name="angle left" />,
+                          icon: true,
+                        }}
+                        nextItem={{
+                          content: <Icon name="angle right" />,
+                          icon: true,
+                        }}
+                        defaultActivePage={1}
+                        totalPages={Math.ceil(this.state.allData.length / 4)}
+                        onPageChange={this.onChangePage}
+                      />
+                    </Table.HeaderCell>
+                  </Table.Row>
+                </Table.Footer>
+              </Table>
+            </div>
+          </div>
+
+          {this.state.showModalAdd && (
+            <SavingAdd
+              showModal={this.state.showModalAdd}
+              handleCloseModal={()=>this.setState({ showModalAdd: !this.state.showModalAdd })}
+              handleSubmit={this.handleAddExpense}
+            />
+          )}
+          {this.state.showModalEdit && (
+            <SavingEdit
+              showModal={this.state.showModalEdit}
+              handleCloseModal={()=>this.setState({ showModalEdit: !this.state.showModalEdit })}
+              handleSubmit={this.handleEditExpense}
+              data={this.state.dataEdit}
+            />
+          )}
+          {this.state.showModalRemove && (
+            <SavingRemove
+              showModal={this.state.showModalRemove}
+              handleCloseModal={()=>this.setState({ showModalRemove: !this.state.showModalRemove })}
+              handleSubmit={this.handleRemoveExpense}
+              data={this.state.dataRemove}
+            />
+          )}
         </div>
       </>
     );
