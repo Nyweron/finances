dd2e5e059f1347b184ea005a4f0173239b966116
Przemysław Juśkiewicz <nyweron@gmail.com>
2022-02-08 21:11:30 +0100

    Create more generic methods

--- src/lib/genericService.js
+++ src/lib/genericService.js
@@ -2,5 +2,62 @@ import { backendUrl } from "../shared/apiUrl";
 
 export const getAll = (controller) => {
     return fetch(backendUrl + "" + controller).then((res) => res.json());
+};
 
+export const create = async (obj, controllerName) => {
+  return await fetch(backendUrl + controllerName, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "POST",
+    body: JSON.stringify(obj),
+  })
+    .then((res) => {
+      if (!res.ok) throw Error(res.statusText);
+      //console.log(res.status);
+      return res.status;
+    })
+    .then((data) => {
+      //console.log(data);
+      return data;
+    })
+    .catch((error) => console.log(error));
+};
+
+export const edit = async (obj, controllerName) => {
+  console.log("temp1", obj);
+  console.log("temp2", obj.id);
+  //console.log("temp2", JSON.stringify(temp));
+  return await fetch(backendUrl + controllerName + "/" + obj.id, {
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    method: "PUT",
+    body: JSON.stringify(obj),
+  })
+    .then((res) => {
+      if (!res.ok) throw Error(res.statusText);
+      //console.log(res.status);
+      return res.status;
+    })
+    .then((data) => {
+      //console.log(data);
+      return data;
+    })
+    .catch((error) => console.log(error));
+};
+
+export const remove = async (id, controllerName) => {
+  return await fetch(backendUrl + controllerName+ "/" + id, {
+    method: "DELETE",
+  })
+    .then((res) => {
+      if (!res.ok) throw Error(res.statusText);
+      return res.status;
+    })
+    .catch((err) => {
+      console.error(err);
+    });
 };
