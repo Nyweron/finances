197bc4d175a017245a869267ca9690a74fa979e8
Przemysław Juśkiewicz <nyweron@gmail.com>
2022-02-15 21:25:18 +0100

    Added redux Actions file

--- src/redux/reducers/modalEdit.js
+++ src/redux/reducers/modalEdit.js
@@ -1,9 +1,11 @@
+import {OPEN_MODAL_EDIT, CLOSE_MODAL_EDIT} from '../actions/actions'
+
 const modalEdit = (state = false, action) => {
 console.log("🚀 ~ file: modalEdit.js ~ line 2 ~ modalEdit ~ action", action)
   switch(action.type) {
-     case 'OPEN_MODAL_EDIT':
+     case OPEN_MODAL_EDIT:
        return true;
-     case 'CLOSE_MODAL_EDIT':
+     case CLOSE_MODAL_EDIT:
        return false;
      default:
        return state;
